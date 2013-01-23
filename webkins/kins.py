23e62e967ccb3115ed0a1a61ae7e2e239791a806
unicorim <freddigbjk@gmail.com>
2013-01-23 16:26:56 +1100

    tries to detect where the url is

--- webkins/kins.py
+++ webkins/kins.py
@@ -1,18 +1,18 @@
 from webob.dec import wsgify
 from paste import httpserver
 from paste.proxy import TransparentProxy
+import urlparse
 
 class HTTPMiddleware(object):
-    """
-    serializes every request and response
-    """
-
     def __init__(self, app):
         self._app = app
 
     @wsgify
     def __call__(self, req):
-        result = req.get_response(self._app)
-        return result
+        url_bits = urlparse.urlsplit(req.url)
+        if "facebook.com" in url_bits.netloc:
+            print "LOL! FACEBOOK"
+        else:
+            return req.get_response(self._app)
 
 httpserver.serve(HTTPMiddleware(TransparentProxy()), "0.0.0.0", port=1024)

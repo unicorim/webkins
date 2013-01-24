d76329619cdd640ad809b0c6d8ba94cefdcfb901
unicorim <freddigbjk@gmail.com>
2013-01-24 19:42:01 +1100

    todo: import all the scripts properly

---
+++ webkins/__init__.py
@@ -0,0 +1,10 @@
+
+# -*- coding: utf-8 -*-
+
+from . import *
+
+__title__ = 'webkins'
+__version__ = '0.1.4'
+__author__ = 'unicorim'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2013 unicorim'

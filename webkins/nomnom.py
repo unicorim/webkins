c3ea6e4daadc3d2593484fe9581b23bd6148ff54
unicorim <freddigbjk@gmail.com>
2013-01-26 13:46:55 +1100

    dont really need classes

--- webkins/nomnom.py
+++ webkins/nomnom.py
@@ -1,29 +1,16 @@
 import re
 
-class Nomnom:
-    def __init__(self,lexicon_luther):
-        self.lex = lexicon_luther
+def validate(self):
+    alpha = str.find(self.lex, "==Napkin==")
+    omega = str.find(self.lex, "==/Napkin==")
+    if alpha and omega:
+        lex_luther = word[alpha+len("==Napkin=="):omega-1].strip()
+        lex_luther = re.sub(re.compile("#.*?"), "", lex_luther)
+        return lex_luther
+    else:
+        return "Stick to the Napkin guidelines please"
     
-    def validate(self):
-        alpha = str.find(self.lex, "==Napkin==")
-        omega = str.find(self.lex, "==/Napkin==")
-        if alpha and omega:
-            lex_luther = word[alpha+len("==Napkin=="):omega-1].strip()
-            lex_luther = re.sub(re.compile("#.*?"), "", lex_luther)
-            return lex_luther
-        else:
-            return "Stick to the Napkin guidelines please"
-    
-    def alloc(self):
-        var, keywords = {}, validate(self.lex)
-        print keywords
+def failsafe(self):
+    var, keywords = {}, validate(self.lex)
         
-        
-word = """#==Napkin==
-#@blah blah
-#@foo  bar 
-#==/Napkin=="""
-
-x = Nomnom(word)
-print x.validate()
         
